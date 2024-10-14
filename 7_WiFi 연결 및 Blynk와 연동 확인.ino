bbae752f536280a1a4131c99c958b85f6140ea15
kimyw33 <kimyw33@gmail.com>
2024-10-14 23:15:08 +0900

    7번 프로젝트까지 완성

--- 7_WiFi 연결 및 Blynk와 연동 확인.ino
+++ 7_WiFi 연결 및 Blynk와 연동 확인.ino
@@ -1,8 +1,8 @@
 #define BLYNK_PRINT Serial  // 꼭 필요하진 않지만 내부 LED를 깜빡거리는 것 외에, 잘 안결되었을 경우 시리얼 모니터를 통해 연결된 IP, 포트번호, ping 등을 확인하기 위해 추가하였습니다.
 // Blynk 서버와 연결을 위한 정보
-#define BLYNK_TEMPLATE_ID "TMPL689_V4Ouk"                    // Blynk 템플릿 ID, Blynk 클라우드와의 연결을 위해 필요한 고유 ID
-#define BLYNK_TEMPLATE_NAME "SmartVivariumV1"                // Blynk 템플릿 이름, 클라우드에서 사용하는 템플릿의 이름
-#define BLYNK_AUTH_TOKEN "ZZ6kgbjkx892h8YXiIK-ctpuh1IM6ETQ"  // Blynk 인증 토큰, 앱에서 생성한 고유 토큰을 사용하여 기기와 서버를 인증함
+#define BLYNK_TEMPLATE_ID "TMPL6F-uJiLbR"                    // Blynk 템플릿 ID, Blynk 클라우드와의 연결을 위해 필요한 고유 ID
+#define BLYNK_TEMPLATE_NAME "SmartVivariumV2"                // Blynk 템플릿 이름, 클라우드에서 사용하는 템플릿의 이름
+#define BLYNK_AUTH_TOKEN "QFhWpnMI6RYBHF-EtBe89rpkQLcOpPgv"  // Blynk 인증 토큰, 앱에서 생성한 고유 토큰을 사용하여 기기와 서버를 인증함
 #define BUILT_IN_LED 2                                       // 내장된 LED의 핀 번호를 정의합니다. ESP32의 내장 LED는 2번 핀에 연결되어 있습니다.
 
 // 필요한 라이브러리 포함
@@ -11,7 +11,7 @@
 #include <BlynkSimpleEsp32.h>  // Blynk와 ESP32 간의 연결을 간단히 구현하기 위한 라이브러리
 
 // WiFi 정보
-char ssid[] = "3층 메이커실";     // 연결할 WiFi의 SSID(네트워크 이름)
+char ssid[] = "3층메이커실";     // 연결할 WiFi의 SSID(네트워크 이름)
 char pass[] = "ksm1234567";    // 연결할 WiFi의 비밀번호
 
 void setup() {
