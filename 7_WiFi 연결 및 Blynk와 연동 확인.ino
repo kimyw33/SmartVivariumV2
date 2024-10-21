7036a7a091108bd1856bfe50079abbf3f6ffd845
kimyw33 <kimyw33@gmail.com>
2024-10-21 11:10:46 +0900

    7번 프로젝트까지 점검

--- 7_WiFi 연결 및 Blynk와 연동 확인.ino
+++ 7_WiFi 연결 및 Blynk와 연동 확인.ino
@@ -1,4 +1,5 @@
 #define BLYNK_PRINT Serial  // 꼭 필요하진 않지만 내부 LED를 깜빡거리는 것 외에, 잘 안결되었을 경우 시리얼 모니터를 통해 연결된 IP, 포트번호, ping 등을 확인하기 위해 추가하였습니다.
+
 // Blynk 서버와 연결을 위한 정보
 #define BLYNK_TEMPLATE_ID "TMPL6F-uJiLbR"                    // Blynk 템플릿 ID, Blynk 클라우드와의 연결을 위해 필요한 고유 ID
 #define BLYNK_TEMPLATE_NAME "SmartVivariumV2"                // Blynk 템플릿 이름, 클라우드에서 사용하는 템플릿의 이름
@@ -11,14 +12,13 @@
 #include <BlynkSimpleEsp32.h>  // Blynk와 ESP32 간의 연결을 간단히 구현하기 위한 라이브러리
 
 // WiFi 정보
-char ssid[] = "3층메이커실";     // 연결할 WiFi의 SSID(네트워크 이름)
+char ssid[] = "3층 메이커실";     // 연결할 WiFi의 SSID(네트워크 이름)
 char pass[] = "ksm1234567";    // 연결할 WiFi의 비밀번호
 
 void setup() {
-  Serial.begin(9600);                         // 시리얼 통신 시작, 9600bps의 속도로 설정하여 디버깅을 위해 시리얼 통신 준비
+  Serial.begin(9600);                         // 시리얼 통신 속도를 9600bps로 설정
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);  // Blynk 서버에 WiFi를 통해 연결, 인증 토큰, SSID, 비밀번호를 사용하여 인증
-
-  pinMode(BUILT_IN_LED, OUTPUT);  // 내장된 LED 핀을 출력 모드로 설정 (HIGH 또는 LOW 값을 통해 LED 켜기/끄기 가능)
+  pinMode(BUILT_IN_LED, OUTPUT);              // 내장된 LED 핀을 출력 모드로 설정
 }
 
 void loop() {
